ea0ae52b5d5307059865066875a31aa9785ec0ac
rmiri <re_miriuk@hotmail.com>
2020-01-08 14:29:58 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def self.clear #didn't check if this method works
+        @@all = []
+    end
 end
